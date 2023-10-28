b076eb81ee120d346184cf5ae7a21ae441b28713
U. Mariano Gálvez <umg@labqtz.local>
2023-10-28 08:01:50 -0600

    vic

---
+++ src/holam/Holam.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holam;
+
+/**
+ *
+ * @author umg
+ */
+public class Holam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        // TODO code application logic here
+    }
+    
+}
